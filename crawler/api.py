3a95f4c927aaba48c0ec731279025d6fb737339c
Oleksandr Mukhopad <alex.mukhopad@gmail.com>
2019-02-04 12:46:35 +0200

    Add logs and refactor

--- crawler/api.py
+++ crawler/api.py
@@ -1,19 +1,24 @@
 import json
 import requests
 
+from datetime import datetime
+from time import sleep
+
 base_site = 'https://developers.ria.com/dom'
 api_key = 'KepwQJaddqwLL4hzvOQXF4DQzicr2WOXDHee5TaH'
 
 
-def http_get(request: str, params={}) -> str:
+def http_get(request: str, params={}, wait_onfail_min=60) -> dict:
     params = {**params, **token()}
     r = requests.get(f'{base_site}{request}', params)
-    print(f'[{r.status_code} {r.reason}] Request {r.url}')
+    log(f'[{r.status_code} {r.reason}] Request {r.url}')
 
-    if r.status_code == 200:
-        return json.loads(r.text)
+    if r.status_code == 429:
+        log(f'Error {r.status_code}: waiting {wait_onfail_min} minutes.')
+        sleep(wait_onfail_min * 60)
+        return http_get(request, params)
 
-    return ''
+    return json.loads(r.text)
 
 
 def token() -> dict:
@@ -21,3 +26,8 @@ def token() -> dict:
         'lang_id': 4,
         'api_key': api_key
     }
+
+
+def log(msg: str):
+    timestamp = str(datetime.now())
+    print(f'[{timestamp}] {msg}')
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -16,32 +16,8 @@ apartments_for_rent_params = {
 def crawl():
     ids = all_apartments_ids()
     for apartment_id in ids:
-        apartment: dict = api.http_get(f'/info/{apartment_id}')
-        price = apartment.get('price', 0)
-        if apartment.get('currency_type', 'грн') == '$':
-            price *= usd_rate
-        characteristics_values = apartment.get('characteristics_values', {})
-        features = [apartment_id,
-                    apartment.get('total_square_meters', ''),
-                    apartment.get('rooms_count', ''),
-                    apartment.get('floor', ''),
-                    apartment.get('floors_count', ''),
-                    apartment.get('wall_type_uk', apartment.get('wall_type', '')),
-                    apartment.get('street_name_uk', apartment.get('street_name', '')),
-                    apartment.get('district_name_uk', apartment.get('district_name', '')),
-                    apartment.get('city_name_uk', apartment.get('city_name', '')),
-                    apartment.get('state_name_uk', apartment.get('state_name', '')),
-                    apartment.get('metro_station_name_uk', apartment.get('metro_station_name', '')),
-                    apartment.get('metro_station_brunch', ''),
-                    bool(characteristics_values.get('1480', '')),  # furnished
-                    bool(characteristics_values.get('1478', '')),  # with heating
-                    bool(characteristics_values.get('1479', '')),  # with repair
-                    bool(characteristics_values.get('1489', '')),  # with balcony
-                    bool(characteristics_values.get('1481', '')),  # jacuzzi
-                    bool(apartment.get('with_panoramas', '')),
-                    apartment.get('publishing_date', ''),
-                    int(price)
-                    ]
+        apartment = api.http_get(f'/info/{apartment_id}')
+        features = parse_apartment(apartment_id, apartment)
         print(features)
         append2csv(features)
 
@@ -69,6 +45,34 @@ def load_apartment_ids() -> List[str]:
         all_apartments += apartments_per_page
 
     return all_apartments
+
+
+def parse_apartment(apt_id: str, apt: dict):
+    price = apt.get('price', apt.get('price_total', 0))
+    if apt.get('currency_type', 'грн') == '$':
+        price *= usd_rate
+    features = apt.get('characteristics_values', {})
+    return [apt_id,
+            apt.get('total_square_meters', ''),
+            apt.get('rooms_count', ''),
+            apt.get('floor', ''),
+            apt.get('floors_count', ''),
+            apt.get('wall_type_uk', apt.get('wall_type', '')),
+            apt.get('street_name_uk', apt.get('street_name', '')),
+            apt.get('district_name_uk', apt.get('district_name', '')),
+            apt.get('city_name_uk', apt.get('city_name', '')),
+            apt.get('state_name_uk', apt.get('state_name', '')),
+            apt.get('metro_station_name_uk', apt.get('metro_station_name', '')),
+            apt.get('metro_station_brunch', ''),
+            bool(features.get('1480', '')),  # furnished
+            bool(features.get('1478', '')),  # with heating
+            bool(features.get('1479', '')),  # with repair
+            bool(features.get('1489', '')),  # with balcony
+            bool(features.get('1481', '')),  # jacuzzi
+            bool(apt.get('with_panoramas', '')),
+            apt.get('publishing_date', ''),
+            int(price)
+            ]
 
 
 def append2csv(columns: List[str]):
